a583b9ef63a83328f186cb5d7642799177d94b3b
markeesh <67460488+markeesh@users.noreply.github.com>
2024-02-24 17:32:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var can = document.getElementById('canvas1');
+var ctx = can.getContext('2d');
+
+ctx.lineWidth = 5;
+
+// linear gradient from start to end of line
+var grad= ctx.createLinearGradient(50, 50, 150, 325);
+grad.addColorStop(0, "blue");
+grad.addColorStop(0.5, "purple");
+grad.addColorStop(1, "yellow");
+
+ctx.strokeStyle = grad;
+
+ctx.beginPath();
+ctx.moveTo(0,0);
+ctx.lineTo(1500,0);
+
+ctx.stroke();
+
+function rotateFunction(){
+    var min = 1024;
+    var max = 9999;
+    var deg = Math.floor(Math.random() * (max - min)) + min;
+    document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+  }
+
+  var element = document.getElementById('mainbox');
+  element.classList.remove('animate');
+  setTimeout(function(){
+    element.classList.add('animate');
+  }, 5000);
